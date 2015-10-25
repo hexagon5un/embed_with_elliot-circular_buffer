f33be289cb8738c8aa881feaf8aeadc783984841
Elliot Williams <elliot@pre.postero.us>
2015-10-25 23:43:20 +0100

    Simplest possible loopback example
    Fixed library to remove atomic block for arduinos

--- main.c
+++ main.c
@@ -44,19 +44,12 @@ int main(void) {
 
 	while (1) {
 
-		// Check if the last character typed was a space
-		status = bufferPeek(&rx_buffer, &input_byte);
-		if (status == BUFFER_OK && input_byte == ' '){
-			while(1) {	
-				status = bufferRead(&rx_buffer, &input_byte);
-				if (status == BUFFER_OK){
-					bufferWrite(&tx_buffer, input_byte);
-				} else {
-					break;
-				}
-			} 
+		status = bufferRead(&rx_buffer, &input_byte);
+		if (status == BUFFER_OK){
+			bufferWrite(&tx_buffer, input_byte);
 			enable_transmission();
 		}
+
 	}                                                  /* End event loop */
 	return 0;                              /* This line is never reached */
 }

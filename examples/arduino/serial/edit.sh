3094066bf0cfb89e9a3f801241a2204c8b7eb22c
Jason R. Jones <jjones@updsys.com>
2016-02-06 17:12:49 -0500

    Adding shell script and make file for all of those who are using AVR-GCC

---
+++ examples/arduino/serial/edit.sh
@@ -0,0 +1,2 @@
+ctags * /usr/avr/include/avr/iomx8.h /usr/avr/include/avr/interrupt.h /usr/avr/include/avr/eeprom.h /usr/avr/include/avr/pgmspace.h /usr/avr/include/avr/sleep.h /usr/avr/include/avr/wdt.h /usr/avr/include/avr/power.h /usr/avr/include/util/delay.h /usr/avr/include/util/atomic.h
+vim *.c *.h Makefile
